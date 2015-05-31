e62969da1a2ac934fa39ad8b5618ebfb8bd62fb3
miamarti <miller.augusto@gmail.com>
2015-05-31 18:27:03 -0300

    cURL Token Authentication

--- application/app.js
+++ application/app.js
@@ -1,7 +1,18 @@
 var config = require('../config'),
     express = require('express'),
     http = require('http'),
-    app = express();
+    app = express(),
+    session = require('express-session');
 
-require("./core")(app, config);
+    app.post('/auth', require('body-parser').json(), function(req, res){
+        if(req.body._token === config.token){
+            require("./core")(app, config);
+            res.json({auth: true});
+        } else {
+            res.json({auth: false});
+        }
+    });
+
 http.createServer(app).listen(3003);
+
+console.log('Your application is running !!!\nPlease performs authentication by sending the token as cURL below:\n$ curl \'http://localhost:3003/auth\' -X POST -H \'Content-Type: application/json\' --data-binary \'{"_token":"7e97189b91ec52a6274943c3277f57ac"}\'\n\n');
--- config.js
+++ config.js
@@ -4,6 +4,18 @@ var Host = require('./application/defaultHost')({
 	database: 'test'
 });
 
+/*
+ * cURL Token Authentication
+ * $ curl 'http://localhost:3003/auth'
+ * -X POST
+ * -H 'Content-Type: application/json'
+ * --data-binary '{"_token":"7e97189b91ec52a6274943c3277f57ac"}'
+ */
+exports.token = '7e97189b91ec52a6274943c3277f57ac';
+
+/*
+ * Application Domains
+ */
 exports.domains = [
     new Host('financial'),
     new Host('reception'),
